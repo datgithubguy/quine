d1410e058d7fb135567ea9c52e00febbc2899eba
Bilal AL-KHANI <bal-khan@e1r9p4.42.fr>
2018-02-13 09:38:34 +0100

    segfaults dans /tmp

--- S/Sully.s
+++ S/Sully.s
@@ -14,7 +14,7 @@ strname: resb 16
 command_compile_run: resb 101
 
 section .data
-format_full_file: db "global start%1$cextern _fprintf%1$cextern _exit%1$cextern _fopen%1$cextern _system%1$cextern _sprintf%1$cextern _memcpy%1$cextern _strcmp%1$c%1$csection .bss%1$cname_exec: resb 16%1$cname_src: resb 16%1$cstrname: resb 16%1$ccommand_compile_run: resb 101%1$c%1$csection .data%1$cformat_full_file: db %2$c%3$s%2$c, 0%1$c%1$cformat_command: db %2$c%4$s%2$c, 0%1$ci: dd %5$d%1$cformat_exec: db %2$c%%s_%%d%%c%2$c, 0%1$cformat_src: db %2$c%%s.s%2$c, 0%1$cread: db %2$cw+%2$c, 0%1$cname_file: db __FILE__, 0%1$cname_first_file: db %2$cSully.s%2$c, 0%1$cname_first_decrement: db %2$cSully_5.s%2$c, 0%1$c%1$csection .text%1$cstart:%1$c%1$cmov al, [rel i]%1$cmov bl, 0%1$cand bl, al%1$ccmp al, bl%1$cje out%1$c%1$c;push rbp%1$c%1$c;xor rax, rax%1$c;lea rdi, [rel format_dbg]%1$c;mov rsi, rcx%1$c;mov rdx, r8%1$c;call _printf%1$c%1$cxor rax, rax%1$clea rdi, [rel strname]%1$clea rsi, [rel name_file]%1$cmov rdx, 5%1$ccall _memcpy%1$c%1$cxor rax, rax%1$clea rdi, [rel name_file]%1$clea rsi, [rel name_first_file]%1$ccall _strcmp%1$c%1$cmov rdi, 0%1$ccmp rdi, rax%1$cje dont_decrement_i%1$c%1$cdec byte [rel i]%1$cdont_decrement_i:%1$c%1$cxor rax, rax%1$clea rdi, [rel name_exec]%1$clea rsi, [rel format_exec]%1$clea rdx, [rel strname]%1$cmov rcx, [rel i]%1$cmov r8, 0%1$ccall _sprintf%1$c%1$cxor rax, rax%1$clea rdi, [rel name_src]%1$clea rsi, [rel format_src]%1$clea rdx, [rel name_exec]%1$ccall _sprintf%1$c%1$cxor rax, rax%1$clea rdi, [rel command_compile_run]%1$clea rsi, [rel format_command]%1$clea rdx, [rel name_src]%1$clea rcx, [rel name_exec]%1$cmov r8, 0%1$ccall _sprintf%1$c%1$cxor rax, rax%1$clea rdi, [rel name_src]%1$clea rsi, [rel read]%1$ccall _fopen%1$c%1$cpop rbp%1$c%1$cpush qword rbp%1$cmov rbp, rsp%1$csub rsp, 16%1$cxor rsi, rsi%1$cmov qword rsi, qword [rel i]%1$cmov qword [rel rbp - 16], rsi%1$cmov rdi, rax%1$cmov rsi, format_full_file%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, format_full_file%1$clea r9, [rel format_command]%1$c%1$ccall _fprintf%1$cadd rsp, 16%1$c%1$cxor rax, rax%1$clea rdi, [rel command_compile_run]%1$ccall _system%1$c%1$cout:%1$c%1$cmov rdi, 0%1$ccall _exit%1$c", 0
+format_full_file: db "global start%1$cextern _fprintf%1$cextern _exit%1$cextern _fopen%1$cextern _system%1$cextern _sprintf%1$cextern _memcpy%1$cextern _strcmp%1$c%1$csection .bss%1$cname_exec: resb 16%1$cname_src: resb 16%1$cstrname: resb 16%1$ccommand_compile_run: resb 101%1$c%1$csection .data%1$cformat_full_file: db %2$c%3$s%2$c, 0%1$c%1$cformat_command: db %2$c%4$s%2$c, 0%1$ci: dd %5$d%1$cformat_exec: db %2$c%%s_%%d%%c%2$c, 0%1$cformat_src: db %2$c%%s.s%2$c, 0%1$cread: db %2$cw+%2$c, 0%1$cname_file: db __FILE__, 0%1$cname_first_file: db %2$cSully.s%2$c, 0%1$cname_first_decrement: db %2$cSully_5.s%2$c, 0%1$c%1$csection .text%1$cstart:%1$c%1$c;push rbp%1$c%1$cmov al, [rel i]%1$cmov bl, 0%1$cand bl, al%1$ccmp al, bl%1$cje out%1$c%1$c;xor rax, rax%1$c;lea rdi, [rel format_dbg]%1$c;mov rsi, rcx%1$c;mov rdx, r8%1$c;call _printf%1$c%1$cxor rax, rax%1$clea rdi, [rel strname]%1$clea rsi, [rel name_file]%1$cmov rdx, 5%1$ccall _memcpy%1$c%1$cxor rax, rax%1$clea rdi, [rel name_file]%1$clea rsi, [rel name_first_file]%1$ccall _strcmp%1$c%1$cmov rdi, 0%1$ccmp rdi, rax%1$cje dont_decrement_i%1$c%1$cdec byte [rel i]%1$cdont_decrement_i:%1$c%1$cxor rax, rax%1$clea rdi, [rel name_exec]%1$clea rsi, [rel format_exec]%1$clea rdx, [rel strname]%1$cmov rcx, [rel i]%1$cmov r8, 0%1$ccall _sprintf%1$c%1$cxor rax, rax%1$clea rdi, [rel name_src]%1$clea rsi, [rel format_src]%1$clea rdx, [rel name_exec]%1$ccall _sprintf%1$c%1$cxor rax, rax%1$clea rdi, [rel command_compile_run]%1$clea rsi, [rel format_command]%1$clea rdx, [rel name_src]%1$clea rcx, [rel name_exec]%1$cmov r8, 0%1$ccall _sprintf%1$c%1$cxor rax, rax%1$clea rdi, [rel name_src]%1$clea rsi, [rel read]%1$ccall _fopen%1$c%1$cpop rbp%1$c%1$cpush qword rbp%1$cmov rbp, rsp%1$csub rsp, 16%1$cxor rsi, rsi%1$cmov qword rsi, qword [rel i]%1$cmov qword [rel rbp - 16], rsi%1$cmov rdi, rax%1$cmov rsi, format_full_file%1$cmov rdx, 10%1$cmov rcx, 34%1$cmov r8, format_full_file%1$clea r9, [rel format_command]%1$c%1$ccall _fprintf%1$cadd rsp, 16%1$c%1$cxor rax, rax%1$clea rdi, [rel command_compile_run]%1$ccall _system%1$c%1$cout:%1$c%1$cmov rdi, 0%1$ccall _exit%1$c", 0
 
 format_command: db "nasm -f macho64 %1$s && ld -macosx_version_min 10.7.0 -lSystem -o %2$s %2$s.o && ./%2$s &%3$c", 0
 i: dd 5
@@ -28,13 +28,13 @@ name_first_decrement: db "Sully_5.s", 0
 section .text
 start:
 
+;push rbp
+
 mov al, [rel i]
 mov bl, 0
 and bl, al
 cmp al, bl
 je out
-
-;push rbp
 
 ;xor rax, rax
 ;lea rdi, [rel format_dbg]
